c73f7796a69b507457841219f8d18cbcbe7cf513
Sridhar <sridhar.sathasivam.tpr.@in.pwc.com>
2023-01-30 13:55:56 +0530

    my subscription order page work

---
+++ app/code/Ecomm/Subscription/Controller/Myaccount/SubscriptionOrder.php
@@ -0,0 +1,33 @@
+<?php
+namespace Ecomm\Subscription\Controller\Myaccount;
+
+class SubscriptionOrder extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+    /**
+     * View page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $dataId = $this->getRequest()->getParam('id');
+        print_r($dataId);exit('done');
+        return $this->_pageFactory->create();
+    }
+}
